37f791ea8210720f64add8b2474f09f40a1f46f6
rowanna <rowankimna@gmail.com>
2024-09-03 10:34:15 +0900

    [study] useEffect 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+  console.log('i run all the time........')
+
+  const iRunOnlyOnce = () => {
+    console.log('just once====')
+  }
+  useEffect(() => {
+    console.log('CALL THE API...')
+  }, [])
   return (
     <div>
       <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"hihi"}/>
+      <button onClick={onClick}>click me</button>
+      {counter}
     </div>
   );
 }
