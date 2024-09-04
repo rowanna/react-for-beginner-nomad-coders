bec3196ff2930a112ede48d68adda785b7f0866d
rowanna <rowankimna@gmail.com>
2024-09-04 10:38:45 +0900

    [study] useEffect 파일 수정

--- src/UseEffectApp.js
+++ src/UseEffectApp.js
@@ -1,8 +1,19 @@
-import Button from "./Button";
 import styles from "./App.module.css"
 import { useEffect, useState } from "react";
 
-function App() {
+
+function Hello() {
+  useEffect(()=> {
+    console.log('created!!!!!!')
+    return () => console.log("destroyed :(@@@@@@@@") // useEffect의 cleanUp. 개념
+  }, [])
+  return <h1>Hello</h1>
+}
+
+
+function UseEffectApp() {
+  const [showing, setShowing] = useState(false);
+  const onClickShowing = () => setShowing((prev) => !prev)
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState('');
   const onClick = () => setCounter((prev) => prev + 1)
@@ -26,8 +37,11 @@ function App() {
       <input value={keyword} onChange={onChange}></input>
       <button onClick={onClick}>click me</button>
       {counter}
+
+      {showing? <Hello /> : null}
+      <button onClick={onClickShowing}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
 
-export default App;
+export default UseEffectApp;
