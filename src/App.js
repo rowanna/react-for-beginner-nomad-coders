88fba1bc8cf7071f171cd258ae6e365029f47fa8
rowanna <rowankimna@gmail.com>
2024-09-05 16:04:19 +0900

    [study] 영화 API fetch, useState, useEffect 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import { useState } from "react";
-import CoinTracker from "./CoinTracker";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <CoinTracker />
-    </div>
-  )
+    const [loading, setLoading] = useState(true)
+    const [movies, setMovies] = useState([])
+
+
+    const getMovies = async () => {
+      const json = await (
+        await fetch(
+          `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+        )
+      ).json();
+
+      setMovies(json.data.movies)
+      setLoading(false)
+    }
+    useEffect(() => {
+      getMovies()
+    }, [])
+
+    // useEffect(() => {
+    //   fetch(
+    //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    //   )
+    //   .then((response)=> response.json())
+    //   .then((json) => {
+    //     setMovies(json.data.movies)
+    //     setLoading(false)
+    //   })
+    // }, [])
+
+
+    return (
+      <div>
+        {loading ? <h1>Loading...</h1> : 
+        movies.map ((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+        
+      </div>
+    );
+  
 }
 
 export default App;
