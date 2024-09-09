1035a447e06443bdc664ef55a719ba8558ca8194
rowanna <rowankimna@gmail.com>
2024-09-09 12:01:47 +0900

    feat: movie컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie.js";
 
 function App() {
     const [loading, setLoading] = useState(true)
@@ -19,32 +20,18 @@ function App() {
       getMovies()
     }, [])
 
-    // useEffect(() => {
-    //   fetch(
-    //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    //   )
-    //   .then((response)=> response.json())
-    //   .then((json) => {
-    //     setMovies(json.data.movies)
-    //     setLoading(false)
-    //   })
-    // }, [])
-
 
     return (
       <div>
         {loading ? <h1>Loading...</h1> : 
         movies.map ((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (
-                <li key={g}>{g}</li>
-              ))}
-            </ul>
-          </div>
+          <Movie 
+          key={movie.id}
+          medium_cover_image={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
+          />
         ))}
 
         
